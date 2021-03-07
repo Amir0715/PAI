da0346606ca5ee3e45880bae9fffb1586bc45afc
Kamolov Amir <45153320+Amir0715@users.noreply.github.com>
2021-03-07 19:49:14 +0300

    ЛР1 (#1)
    * доделал 1 лабу
    
    * Add lab1

--- Lab1/code/main.py
+++ Lab1/code/main.py
@@ -3,17 +3,18 @@ from core.resampling import *
 from core.halftoning import *
 from core.thresholding import *
 
-filename = "test3.jpg"
+filename = "downsampling_text.jpg"
 
 original = Image.open("../res/" + filename)
 
-new_image = resampling(original, 1/2)
-new_image.save("../res/downsampling_" + filename)
-
-new_image = resampling(original, 2)
-new_image.save("../res/upsampling_" + filename)
-
-
+# new_image = resampling(original, 5/2)
+# new_image.save("../res/downsampling_" + filename)
+#
+# new_image = resampling(original, 5)
+# new_image.save("../res/upsampling_" + filename)
+#
+# new_image = resampling(original, 1/4)
+# new_image.save("../res/downsampling_" + filename)
 # new_image = halftone(original)
 # new_image.show()
 # new_image.save("../res/halftone" + filename)
@@ -21,5 +22,7 @@ new_image.save("../res/upsampling_" + filename)
 # new_image = sepia(original, 30)
 # new_image.show()
 # new_image.save("../res/sepia_" + filename)
-
-# new_image = kristian_threshold(original)
+halfton = halftone(original)
+# halfton.show()
+new_image = kristian_threshold(image=halfton, b=15, k=0.2)
+new_image.show()
--- core/thresholding.py
+++ core/thresholding.py
@@ -1,14 +1,38 @@
 from PIL import Image
 import numpy as np
+from progress.bar import Bar
 
-def kristian_threshold(image : Image.Image) -> Image.Image:
-    b = 10 # размер локального окна b*b
+from core.utils import windowing
+from tqdm import tqdm
 
+
+def kristian_threshold(image: Image.Image, k: float = 0.5, b: int = 3) -> Image.Image:
     width = image.size[0]
     height = image.size[1]
 
-    bx : np.ndarray = np.ogrid[0:width:b]
-    np.append(bx, width)
-    print(bx)
+    pix = np.array(image)
+    new_image = np.zeros(shape=(height, width, 3))
+    # new_image = np.full(shape=(height, width, 3), fill_value=255)
+    MinG = pix.min()
+    sigma_m = np.zeros(shape=(height, width, 2))
+    for x in tqdm(range(width)):
+        for y in range(height):
+            x1 = x - int(b / 2) if x - int(b / 2) > 0 else 0
+            x2 = x + int(b / 2) if x + int(b / 2) < width - 1 else width - 1
+            y1 = y - int(b / 2) if y - int(b / 2) > 0 else 0
+            y2 = y + int(b / 2) if y + int(b / 2) < height - 1 else height - 1
 
-    return image
+            local_window = pix[y1:y2 + 1, x1:x2 + 1, 0]
+            sigma_m[y, x, 0] = local_window.mean()
+            sigma_m[y, x, 1] = local_window.std()
+
+    R = sigma_m[:, :, 1].max()
+    T = ((1 - k) * sigma_m[:, :, 0]) + (k * MinG) + (k * (sigma_m[:, :, 1] / R) * (sigma_m[:, :, 0] - MinG))
+    print(T[0, 0])
+    print(pix[0, 0, 0])
+    for x in tqdm(range(width)):
+        for y in range(height):
+            if pix[y, x, 0] > T[y, x]:
+                new_image[y, x] = [255, 255, 255]
+
+    return Image.fromarray(new_image.astype(np.uint8)).convert("RGB")
